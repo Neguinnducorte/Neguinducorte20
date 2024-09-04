67c30dadc0ec98abd663db8b23db64efca82f26f
Neguinnducorte <lucas.maia.souza@escola.pr.gov.br>
2024-09-04 23:13:35 +0000

    🙅🏿‍♂️

--- sketch.js
+++ sketch.js
@@ -1,1 +1,11 @@
+function setup() {
+    createCanvas(400, 400);
+    background("blue");
+}
 
+function draw() {}
+  stroke("white");
+  fill("black");
+
+    if(mouseIsPressed) {}
+    react(mouseX, mouseY, 20, 20);
