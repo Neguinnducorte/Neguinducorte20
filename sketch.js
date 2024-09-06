4ce3e0a6114560d4f540dd52f935af6d2819bf50
Neguinnducorte <lucas.maia.souza@escola.pr.gov.br>
2024-09-06 00:25:32 +0000

    negot7

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
  function setup() {
     createCanvas(500, 500);
-      background("blue");
+      background("#E91E1E");
       }
 
       function draw() {
-        stroke("white");
-          fill("black");
+        stroke("#000A11");
+          fill("#FFFFFF");
             
                  if(mouseIsPressed) {
                       rect(mouseX, mouseY, 20, 20);
                         }
                         }
- 
